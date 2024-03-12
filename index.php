77cb2cdf5545f1a9505f715f61904bdd385a4a5a
Douglassilvadelima <162905202+Douglassilvadelima@users.noreply.github.com>
2024-03-11 23:59:29 -0300

    teste

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang = 'pt-br'>
+<head>
+<meta charset = 'UTF-8'>
+<meta name = 'viewport' content = 'width=device-width, initial-scale=1.0'>
+<title> Tela de login  </title>;
+<link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+<div class ' tela -login'>
+
+<h1>login</h1>
+<form method="POST" action="">
+<input type = 'text' placeholder = 'Nome'>
+<br><br>
+<input type = 'password' placeholder = 'senha'>
+<br><br>
+<button>Enviar</button>
+</form>
+
+
+</div>
+
+</body>
+</html>
